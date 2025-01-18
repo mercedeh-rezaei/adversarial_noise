bd9a5e737045ebb9969205e5299126cb474cfee1
Mercedeh Rezaei <mercedehrezaei@mercedeh.home>
2025-01-18 15:45:44 +0000

    added logic behind fgsm attack fucntion

--- src/attack.py
+++ src/attack.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 def fgsm_attack(model, input_image, target_class, epsilon=0.01):
 
@@ -6,6 +7,21 @@ def fgsm_attack(model, input_image, target_class, epsilon=0.01):
     Performs FGSM attack on the image to a target class
 
     """
-    
 
-    return input_image
+    model.eval()
+    input_image.requires_grad=True
+    output = model(input_image)
+
+    target = torch.tensor([target_class]).to(input_image.device)
+    loss = F.cross_entropy(output, target)
+
+    model.zero_grad()
+    loss.backward()
+
+    perturbation = epsilon * input_image.grad.sign()
+    
+    adversarial_image = input_image + perturbation
+    # adding clipping to keep the range in [0,1]
+    adverarial_impage = torch.clamp(adversarial_image, 0, 1)
+
+    return adverarial_impage
